ebb84278432f82423dac6d1b834ff6acd993e53a
한상필 <tksvlf123@gmail.com>
2024-02-20 08:58:02 +0900

    feat: .gitignore 추가

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(1);
-		// gitignore 설정하기
+		// gitignore 설정하기 : https://demat.tistory.com/55
 	}
 
 }
