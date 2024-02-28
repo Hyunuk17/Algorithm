0ee3857eb33b1c59f6706ece051d2ce9cc25bf40
안현욱 <dksgusdnr17@naver.com>
2024-02-28 18:01:12 +0900

    feat: 2024.02.28(수) 문제풀이 중

---
+++ src/Main.java
@@ -0,0 +1,89 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		/**
+		 * BOJ 20303. 할로윈의 양아치
+		 * ---------------------
+		 * 
+		 * 사탕 빼앗기
+		 * 한 아이에게 빼앗으면 그 친구들의 사탕도 모조리 뺴았는다.
+		 * - 무향 그래프 인접 노드?
+		 * 
+		 * K명 미만으로 최대한 빼앗을 수 있는 사탕 개수 구하기
+		 * 
+		 * N: 거리의 아이들 수, 1<= N <= 30,000
+		 * M: 아이들의 친구 관계 수, 0 <= M <= 100,000
+		 * K: 넘으면 안된는 아이 인원 수, 1 <= K <= min(N, 3,000) 
+		 * 
+		 * c: 아이들이 받은 사탕의  수, 1 <= c_i <=10,000 
+		 */
+		
+		// 입력
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		
+		// 그래프 생성
+		graph = new ArrayList[N+1];
+		for(int i=1;i<=N;i++) {
+			graph[i] = new ArrayList<>();
+		}
+		
+		// 사탕 개수 입력
+		st = new StringTokenizer(br.readLine());
+		c = new int[M+1];
+		for(int i=1;i<=M;i++) {
+			c[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		// 간선 입력
+		for(int i=1;i<=M;i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			graph[a].add(b);
+			graph[b].add(a);
+		}
+
+		// 문제풀이
+		// 모든 노드를 시작점으로 삼아 BFS
+		// 3,000 * (3,000 + 100,000) == 3*10^3 * 10^6 : 완전 탐색 불가능
+		
+		// 시작점을 특정할 수 없음
+		// 해당 연결 요소가 포함한 값의 총합으로 계산? -> BFS, Union-Find
+		
+		
+		// 출력
+		bw.write(sb.toString());
+		bw.flush();
+		
+		bw.close();
+		br.close();
+	}
+	
+	static int N;
+	static int M;
+	static int K;
+	static int[] c;
+	static List<Integer>[] graph;
+	
+	static class Node {
+		int v;
+		int w;
+	}
+
+}
