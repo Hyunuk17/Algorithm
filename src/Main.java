3a7ab132f6bdf0e7812b595bdbd44a7361d2fe41
안현욱 <dksgusdnr17@naver.com>
2024-03-05 16:17:13 +0900

    feat: 2024.03.05(화) 알고리즘 스터디

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,9 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
@@ -13,137 +15,136 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		/**
-		 * BOJ 20303. 할로윈의 양아치 ---------------------
+		 * BOJ 14938. 서강그라운드
+		 * --------------------
 		 * 
-		 * 사탕 빼앗기 한 아이에게 빼앗으면 그 친구들의 사탕도 모조리 뺴았는다. - 무향 그래프 인접 노드?
+		 * 배틀그라운드 비슷한 게임
 		 * 
-		 * K명 미만으로 최대한 빼앗을 수 있는 사탕 개수 구하기
+		 * 낙하한 후, 가장 많은 아이템 먹기
 		 * 
-		 * N: 거리의 아이들 수, 1<= N <= 30,000 M: 아이들의 친구 관계 수, 0 <= M <= 100,000 K: 넘으면 안된는
-		 * 아이 인원 수, 1 <= K <= min(N, 3,000)
-		 * 
-		 * c: 아이들이 받은 사탕의 수, 1 <= c_i <=10,000
+		 * i번 정점에 있는 아이템 j개 주워 가중치 w이내 최대 아이템 줍기 
+		 *
+		 * 지역 N : 1 <= N <= 100
+		 * 범위 M : 1 <= M <= 15
+		 * 길 수 R : 1 <= R <= 100
+		 * 아이템 수 t : 1 <= t <= 30
+		 *
 		 */
-
+		
+		// Dijkstra 문제인 것을 파악했지만, 구현 방법을 몰랐음
+		// 답을 찾아보고 Dijkstra 구현 방법을 숙지
+		
 		// 입력
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringBuilder sb = new StringBuilder();
-
+		
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(st.nextToken());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		r = Integer.parseInt(st.nextToken());
+		
+		
+		
+		t = new int[n+1];
+		st = new StringTokenizer(br.readLine());
+		for(int i=1;i<=n;i++) {
+			t[i] = Integer.parseInt(st.nextToken());
+		}
 
-		// 그래프 생성
-		graph = new ArrayList[N + 1];
-		for (int i = 1; i <= N; i++) {
+		graph = new ArrayList[n+1];
+		for(int i=1;i<=n;i++) {
 			graph[i] = new ArrayList<>();
 		}
-
-		// 사탕 개수 입력
-		st = new StringTokenizer(br.readLine());
-		c = new int[N + 1];
-		for (int i = 1; i <= N; i++) {
-			c[i] = Integer.parseInt(st.nextToken());
-		}
-
-		// 간선 입력
-		for (int i = 1; i <= M; i++) {
+		
+		for(int i=0;i<r;i++) {
 			st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
-			graph[a].add(b);
-			graph[b].add(a);
+			int w = Integer.parseInt(st.nextToken());
+			graph[a].add(new Node(b, w));
+			graph[b].add(new Node(a, w));
 		}
-
+		
 		// 문제풀이
-		// 모든 노드를 시작점으로 삼아 BFS
-		// 3,000 * (3,000 + 100,000) == 3*10^3 * 10^6 : 완전 탐색 불가능
-
-		// 시작점을 특정할 수 없음
-		// 해당 연결 요소가 포함한 값의 총합으로 계산? -> BFS, Union-Find
-		visited = new boolean[N + 1];
-		for (int i = 1; i <= N; i++) {
-			if (visited[i] == false) {
-				BFS(i);
-			}
-		}
-
-		// 최대 인원수가 K미만이 되도록 연결 요소를 조합
-		// 최대 사탕의 수 구하기
-
-		// Knapsack
-		// i(번호), j(무게), dp[i][j](최대캔디수)
-		// dp[i][j] := i번째 물건까지 무게 j 이하로 가방에 담았을 때, 최대 가치
-		dp = new int[list.size() + 1][K];
-
-		for (int i = 1; i <= list.size(); i++) {
-			for (int j = 0; j < K; j++) {
-				int w = list.get(i - 1).cnt;
-				int v = list.get(i - 1).candy;
-
-				if (j - w < 0) {
-					dp[i][j] = dp[i - 1][j];
-					continue;
+		// m은 수색 범위
+		// 시작 정점에서 모든 정점까지 m보다 작은 가중치를 가지는 모든 아이템 합
+		
+		// 다익스트라로 시작 정점에서 모든 정점까지의 가중치를 구한다
+		int max = 0;
+		distance = new int[n+1];
+		for(int i=1;i<=n;i++) {			
+			Dijkstra(i);
+			
+			// 구한 값으로 가중치가 m이하인 정점의 모든 아이템 개수 합을 구한다
+			int sum = 0;
+			for(int j=1;j<=n;j++) {
+				if(distance[j] <= m) {
+					sum += t[j];
 				}
-				dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - w] + v);
 			}
+			
+			// 모든 시작 정점에 대해 최대값을 구한다
+			max = Math.max(max, sum);
 		}
-
+		
 		// 출력
-		sb.append(dp[list.size()][K - 1]);
+		sb.append(max);
 		bw.write(sb.toString());
 		bw.flush();
-
+		
 		bw.close();
 		br.close();
 	}
 
-	static int N;
-	static int M;
-	static int K;
-	static int[] c;
-	static List<Integer>[] graph;
-	static boolean[] visited;
-	static List<Node> list = new ArrayList<>();
-	static int max = 0;
-	static int[][] dp;
-
-	// Connected Component 구하기
-	static void BFS(int start) {
-		int candy = 0;
-		int cnt = 0;
-		Queue<Integer> queue = new ArrayDeque<>();
-		queue.add(start);
-		visited[start] = true;
-
-		while (!queue.isEmpty()) {
-			int now = queue.poll();
-			candy += c[now];
-			cnt++;
-
-			for (int next : graph[now]) {
-				if (visited[next] == true) {
+	static int n;
+	static int m;
+	static int r;
+	static int[] t;
+	static List<Node>[] graph;
+	static int[] distance;
+	
+	static void Dijkstra(int start) {
+		// 최댓값 초기화
+		Arrays.fill(distance, Integer.MAX_VALUE);
+		distance[start]  = 0; // 자기 자신은 가중치 0
+		
+		PriorityQueue<Node> pq = new PriorityQueue<>();
+		pq.add(new Node(start, 0)); // 시작 정점
+		
+		while(!pq.isEmpty()) {
+			Node cur = pq.poll();
+						
+			// queue에서 pop되었을 때를 기준으로 방문처리 - 가중치가 최소
+			if(cur.w > distance[cur.v]) { // 이미 방문한 정점 - 다른 최단 경로가 존재
+				continue;
+			}
+			
+			for(Node next : graph[cur.v]) {
+				
+				if(distance[next.v] < distance[cur.v] + next.w) { // 최단 거리가 아님
 					continue;
 				}
-
-				visited[next] = true;
-				queue.add(next);
+				
+				distance[next.v] = distance[cur.v] + next.w; // 가중치 갱신
+				pq.add(new Node(next.v, distance[next.v]));
 			}
 		}
-
-		list.add(new Node(candy, cnt));
 	}
+	
+	
+	static class Node implements Comparable<Node>{
+		int v;
+		int w;
+		
+		public Node(int v, int w) {
+			this.v = v;
+			this.w = w;
+		}
 
-	static class Node {
-		int candy;
-		int cnt;
-
-		public Node(int candy, int cnt) {
-			this.candy = candy;
-			this.cnt = cnt;
+		@Override
+		public int compareTo(Node o) {
+			return this.w - o.w;
 		}
 	}
-
 }
