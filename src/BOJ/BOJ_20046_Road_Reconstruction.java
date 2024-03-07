503c46ea22380961d467eee4652d97972a5f4010
Hyunuk <dksgusdnr17@naver.com>
2024-03-07 20:18:00 +0900

    feat: 2023.03.07(목) 알고리즘 스터디

--- src/BOJ/BOJ_20046_Road_Reconstruction.java
+++ src/BOJ/BOJ_20046_Road_Reconstruction.java
@@ -1,3 +1,4 @@
+package BOJ;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -7,7 +8,7 @@ import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
-public class Main {
+public class BOJ_20046_Road_Reconstruction {
 
 	public static void main(String[] args) throws IOException {
 		/**
